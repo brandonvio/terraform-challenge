2a353c28fa71c560a353d7a42ddeb4f9d904516a
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2020-03-27 01:39:11 -0700

    first commit

--- main.tf
+++ main.tf
@@ -157,34 +157,6 @@ resource "aws_cloudwatch_log_group" "get-project-list-log-group" {
   retention_in_days = 30
 }
 
-# resource "aws_iam_policy" "lambda-logging" {
-#   name        = "lambda_logging"
-#   path        = "/"
-#   description = "IAM policy for logging from a lambda"
-
-#   policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Action": [
-#         "logs:CreateLogGroup",
-#         "logs:CreateLogStream",
-#         "logs:PutLogEvents"
-#       ],
-#       "Resource": "arn:aws:logs:*:*:*",
-#       "Effect": "Allow"
-#     }
-#   ]
-# }
-# EOF
-# }
-
-# resource "aws_iam_role_policy_attachment" "lambda-logs" {
-#   role       = aws_iam_role.iam_for_lambda.name
-#   policy_arn = aws_iam_policy.lambda-logging.arn
-# }
-
 #############################################################
 # API Gateway.
 #############################################################
